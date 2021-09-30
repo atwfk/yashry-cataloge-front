5f51c3f8c605f26af62b1f31bf5d290f2f9a90b1
ahmed <ahmed.twfiek8@gmail.com>
2021-09-30 17:58:07 +0200

    fetch products from getServerSideProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
-import type { NextPage } from "next";
-import type { ReactElement } from "react";
+import type { GetServerSideProps } from "next";
+import type { FC, ReactElement } from "react";
 import Head from "next/head";
 import CategoryPage from "@modules/CategoryPage/CategoryPage";
+import { transformQueries } from "@modules/shared/logic/transformQueries/transformQueries";
+import { getProducts } from "@modules/CategoryPage/api/getProducts/getProducts";
+import { IData } from "@modules/shared/types/IData";
 
-const Home: NextPage = (): ReactElement => {
+const Home: FC<{ data: IData.IProduct[] }> = ({ data }): ReactElement => {
+  console.log(data);
   return (
     <div>
       <Head>
@@ -12,6 +16,29 @@ const Home: NextPage = (): ReactElement => {
       <CategoryPage />
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { query } = context;
+
+  const queryString = transformQueries(query);
+
+  try {
+    const data = await getProducts(undefined, queryString);
+
+    return {
+      props: { data },
+    };
+  } catch (error: unknown) {
+    const {
+      message,
+      errorCode,
+      error: isError,
+    } = error as { message: string; error: boolean; errorCode: number };
+    return {
+      props: { data: { message, errorCode, error: isError } },
+    };
+  }
 };
 
 export default Home;
