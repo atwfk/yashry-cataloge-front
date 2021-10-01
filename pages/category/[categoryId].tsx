4d3e0478ee2d918e6fc28e969304e4ab890c6749
ahmed <ahmed.twfiek8@gmail.com>
2021-10-02 01:18:42 +0200

    used the products functions to create the application state

--- pages/category/[categoryId].tsx
+++ pages/category/[categoryId].tsx
@@ -5,30 +5,71 @@ import CategoryPage from "@modules/CategoryPage/pages/CategoryPage";
 import { transformQueries } from "@modules/shared/logic/transformQueries/transformQueries";
 import { getProducts } from "@modules/CategoryPage/api/getProducts/getProducts";
 import { IData } from "@modules/shared/types/IData";
+import { getProductsByPrice } from "@modules/shared/logic/getProductsByPrice/getProductsByPrice";
+import {
+  createFilteredColors,
+  getUniqueColors,
+} from "@modules/shared/logic/productsColorsLogic/productsColorsLogic";
+import {
+  createFilteredRatings,
+  getUniqueRatings,
+} from "@modules/shared/logic/ProductsRatingsLogic/ProductsRatingsLogic";
+import { getMinMaxPrices } from "@modules/shared/logic/getMinMaxPrices/getMinMaxPrices";
 
-const Home: FC<{ data: IData.IProduct[] }> = ({ data }): ReactElement => {
+const Category: FC<IData.IProps> = ({ data }): ReactElement => {
+  console.log(data);
   return (
     <div>
       <Head>
         <title>Category | E-commerce</title>
       </Head>
-      <CategoryPage data={data} />
+      <CategoryPage data={data.products} />
     </div>
   );
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { query, params } = context;
-  console.log(query, params);
   const { categoryId } = params as { categoryId: string };
-
+  const { from, to } = query as { from: string; to: string };
   const queryString = transformQueries(query);
 
   try {
     const data = await getProducts(categoryId, queryString);
 
+    const filteredProducts = getProductsByPrice(data, +from, +to);
+
+    const uniqueColors = getUniqueColors(filteredProducts);
+    const filteredColors = createFilteredColors(
+      uniqueColors,
+      query.colors ?? "",
+    );
+
+    const uniqueRatings = getUniqueRatings(filteredProducts);
+    const sortedRatings = uniqueRatings.sort();
+    const filteredRatings = createFilteredRatings(
+      sortedRatings,
+      query.rating ?? "",
+    );
+
+    const minMaxPrice = getMinMaxPrices(filteredProducts);
+
     return {
-      props: { data },
+      props: {
+        data: {
+          products: filteredProducts,
+          uniqueColors,
+          colors: filteredColors,
+          uniqueRatings: sortedRatings,
+          ratings: filteredRatings,
+          prices: {
+            min: minMaxPrice[0],
+            max: minMaxPrice[1],
+            from: from ?? minMaxPrice[0],
+            to: to ?? minMaxPrice[1],
+          },
+        },
+      },
     };
   } catch (error: unknown) {
     const {
@@ -42,4 +83,4 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   }
 };
 
-export default Home;
+export default Category;
