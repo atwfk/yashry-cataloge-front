63b87498a8713525361b2dad2a5e41413af1de1b
ahmed <ahmed.twfiek8@gmail.com>
2021-10-02 01:14:51 +0200

    update shared interfaces

--- modules/shared/types/IData.d.ts
+++ modules/shared/types/IData.d.ts
@@ -1,4 +1,14 @@
 declare namespace IData {
+  export interface IProps {
+    data: {
+      products: IProduct[];
+      uniqueColors: string[];
+      colors: IFilteredColors[];
+      uniqueRatings: number[];
+      ratings: IFilteredStars[];
+      prices: IPrices;
+    };
+  }
   export interface IProduct extends ICategory {
     color: string;
     price: number;
@@ -12,6 +22,25 @@ declare namespace IData {
     id: number;
     name: string;
     image: string;
+  }
+
+  export interface IFilteredColors {
+    id: number;
+    name: string;
+    active: boolean;
+  }
+
+  export interface IFilteredStars {
+    id: number;
+    rating: number;
+    active: boolean;
+  }
+
+  export interface IPrices {
+    min: number;
+    max: number;
+    from: number;
+    to: number;
   }
 }
 
