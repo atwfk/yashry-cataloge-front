925c0a0b7f973b3421bed6e84c71b60f20128df6
ahmed <ahmed.twfiek8@gmail.com>
2021-10-04 01:42:55 +0200

    add loading prop and handle passing empty products array to Peroducts comp

--- modules/shared/components/organisms/Products/Products.test.tsx
+++ modules/shared/components/organisms/Products/Products.test.tsx
@@ -8,7 +8,27 @@ describe("Products", () => {
     render(<Products products={mockedProducts} loading={false} />);
 
     const products = screen.getAllByTestId("product");
+    const loader = screen.queryByTestId("loader");
+    const notFoundProducts = screen.queryByTestId("not-found-products");
 
     expect(products).toHaveLength(4);
+    expect(loader).not.toBeInTheDocument();
+    expect(notFoundProducts).not.toBeInTheDocument();
+  });
+
+  it("should render loading div when passing loading={true}", () => {
+    render(<Products products={[]} loading={true} />);
+
+    const loader = screen.getByTestId("loader");
+
+    expect(loader).toBeInTheDocument();
+  });
+
+  it("should display no products found when we passing empty products array", () => {
+    render(<Products products={[]} loading={false} />);
+
+    const notFoundProducts = screen.queryByTestId("not-found-products");
+
+    expect(notFoundProducts).toBeInTheDocument();
   });
 });
--- modules/shared/components/organisms/Products/Products.tsx
+++ modules/shared/components/organisms/Products/Products.tsx
@@ -8,29 +8,31 @@ const products: FC<{ products: IData.IProduct[]; loading: boolean }> = ({
   products,
   loading,
 }): ReactElement => {
+  if (loading) {
+    return <div data-testid="loader">loading...</div>;
+  }
+
+  if (products.length === 0) {
+    return <div data-testid="not-found-products">Cannot find any product</div>;
+  }
+
   return (
     <div className={styles.products}>
-      {loading ? (
-        <div>loading...</div>
-      ) : (
-        <>
-          {products.map((product) => (
-            <div key={product.id} data-testid="product">
-              <Product
-                price={product.price}
-                rating={product.rating}
-                id={product.id}
-                name={product.name}
-                image={product.image}
-                color={product.color}
-                currency={product.currency}
-                releaseDate={product.releaseDate}
-                categoryId={product.categoryId}
-              />
-            </div>
-          ))}
-        </>
-      )}
+      {products.map((product) => (
+        <div key={product.id} data-testid="product">
+          <Product
+            price={product.price}
+            rating={product.rating}
+            id={product.id}
+            name={product.name}
+            image={product.image}
+            color={product.color}
+            currency={product.currency}
+            releaseDate={product.releaseDate}
+            categoryId={product.categoryId}
+          />
+        </div>
+      ))}
     </div>
   );
 };
